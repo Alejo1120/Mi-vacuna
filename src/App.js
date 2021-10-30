e15da2a211661bd2ac93cf0cc29cb95a744d13e5
Alejo1120 <cedenoalejandro117@gmail.com>
2021-10-30 16:52:10 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router , Route, Switch} from 'react-router-dom';
+//import { BrowserRouter as Router , Route, Switch} from 'react-router-dom';
 import './App.css';
-import Home from './pages/home/home';
-import Login from './pages/login/login';
-import Register from './pages/register/register';
+// import Home from './pages/home/home';
+// import Login from './pages/login/login';
+// import Register from './pages/register/register';
+import imglogo from '../src/assets/images/img-logo.jpg';
 
 function App() {
   return (
-   <Router>
-    <Switch>
-       <Route exact path="/" component={Home} />
-       <Route path="/register" component={Register} />
-       <Route path="/login" component={Login} />
-       
-    </Switch>
-   </Router>
+
+    <>
+    <h1>App js</h1>
+    <img src = {imglogo}/>
+    </>
   );
 }
 
